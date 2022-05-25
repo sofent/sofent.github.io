696f9f91e8906b7eb31aa95379a439a2e991584a
sofent <sofent@qq.com>
2022-05-25 09:18:10 +0000

    auto build

---
+++ assets/index.md.b54c29a8.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r,a}from"./app.85f648d0.js";const b='{"title":"Sofent\u7684Blog","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}',o={},n=a("",4),i=[n];function l(s,_,c,d,f,p){return r(),t("div",null,i)}var u=e(o,[["render",l]]);export{b as __pageData,u as default};
