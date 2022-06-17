ceeed1adfca6691df2476e578b410865240567f5
sofent <sofent@qq.com>
2022-06-17 13:15:56 +0000

    auto build

--- assets/swift_share.md.6d58c720.lean.js
+++
@@ -1,5 +0,0 @@
-import{_ as a,c as l,a as p,e as s,r as o,o as e}from"./app.f0f760f9.js";var t="/assets/target.5b72a1bd.png",c="/assets/target2.7c891240.png";const h='{"title":"\u521B\u5EFAAction Extention","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u65E0UI\u7248\u672C","slug":"\u65E0ui\u7248\u672C"},{"level":2,"title":"\u6709UI\u7248\u672C","slug":"\u6709ui\u7248\u672C"}],"relativePath":"swift/share.md"}',r={name:"swift/share.md"},D=s("",8),F=s("",8);function y(A,C,i,d,g,f){const n=o("Mermaid");return e(),l("div",null,[D,p(n,{id:"mermaid_382ee168",graph:`graph LR
-A[\u56FE\u7247] -->|\u5206\u4EAB|B(Extention)
-    B --> |\u5B58\u50A8|C[UserDefault]
-    C -->|\u6253\u5F00| D[APP]
-    D -->|\u8BFB\u53D6| E[\u540E\u7EED\u5904\u7406]`}),F])}var m=a(r,[["render",y]]);export{h as __pageData,m as default};
